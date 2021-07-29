9b1cb70a45bdaac031dd812d442c001fc47d8437
Karl Mouat <normandypacific@outlook.com>
2021-07-29 21:21:55 +1000

    added posts and comment services tto seperate index.js'

---
+++ blog/comments/index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+const {randomBytes}  = require('crypto');
+app.use(express.urlencoded({extended: true }))
+app.use(express.json())
+
+
+const commentsByPostId = {};
+
+app.get('/posts/:id/comments', (req, res) => {
+  res.send(commentsByPostId[req.params.id] || []);
+});
+
+app.post('/posts/:id/comments', (req, res) => {
+  const commentId = randomBytes(4).toString('hex');
+  const { content } = req.body;
+
+  const comments = commentsByPostId[req.params.id] || [];
+
+  comments.push({ id: commentId, content });
+
+  commentsByPostId[req.params.id] = comments;
+
+  res.status(201).send(comments);
+});
+
+app.listen(4001, () => {
+  console.log('Listening on 4001');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
